1cc8644d0d4d728321fca139039af4a9e03ced17
Mohd Shahril <mohd_shahril_96@yahoo.com>
2016-04-15 01:56:52 +0800

    Add experimental code to export timetable to image

--- scripts/script.js
+++ scripts/script.js
@@ -230,6 +230,18 @@ document.querySelector('.newtable').onchange = function (e) {
     }
 };
 
+document.querySelector('#saveTimeTable').onclick = function () {
+    html2canvas(document.querySelector('.timetable'), {
+        onrendered: function (canvas) {
+            var a = document.createElement('a');
+            // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            a.download = 'somefilename.jpg';
+            a.click();
+        }
+    });
+};
+
 function addNewRow() {
 
     var elems = document.querySelectorAll('.select-subject');
