20a49942db9df41c88f8b053faee76bd2cc8a454
Mohd Shahril <mohd_shahril_96@yahoo.com>
2017-03-20 11:18:33 +0800

    Refactorized and commented codes

--- modules/icress_module.php
+++ modules/icress_module.php
@@ -2,8 +2,8 @@
 
 require_once('./config.php');
 
-// non-selangor UiTMs code
-// refer `istudent_module.php` for description about each code
+# non-selangor UiTMs code
+# refer `istudent_module.php` for description about each code
 $list = array( 'AR', 'SI', 'JK', 'MA', 'SG', 'SP', 'AG', 'KP', 'BT', 'SA', 'KK', 'DU' );
 
 function icress_getJadual() {
@@ -13,20 +13,25 @@ function icress_getJadual() {
     $get = file_get_contents('http://' . ICRESS_URL . '/jadual/jadual/jadual.asp');
     $http_response_header or die("Alert_Error: Icress timeout! Please try again later."); 
 
-    $collect = array();
-    $selangor = array();
+    $collect = [];
+    $selangor = [];
+
+    # extract faculty name and its code
     preg_match_all('/(?<=value=")(\w*)-(.[^"]*)/', $get, $out);
 
-    for ($i = 0; $i < count($out[1]); $i++)
-        if (in_array($out[1][$i], $list))
-            $collect[] = array('code' => $out[1][$i], 'fullname' => $out[2][$i]);
-        else
-            $selangor[] = array($out[1][$i] => $out[2][$i]);
+    for ($i = 0; $i < count($out[1]); $i++) {
 
-    // add dummy code for UiTM inside selangor
+        if (in_array($out[1][$i], $list)) { # if non-selangor uitm
+            $collect[] = array('code' => $out[1][$i], 'fullname' => $out[2][$i]);
+        } else {
+            $selangor[] = array($out[1][$i] => $out[2][$i]);
+        }
+    }
+
+    # add dummy code for UiTM inside selangor
     $collect[] = array('code' => 'B', 'fullname' => 'Kampus Selangor');
 
-    // save selangor's faculties
+    # save selangor's faculties
     file_put_contents("./cache/SELANGOR_FACULTIES.dat", json_encode($selangor));
 
     return json_encode($collect);
@@ -34,15 +39,15 @@ function icress_getJadual() {
 
 function icress_getFaculty($faculty) {
 
-    // special case - if UiTM in selangor
+    # special case - if UiTM in selangor
     if ($faculty == 'B') {
 
         $selangor = json_decode(file_get_contents("./cache/SELANGOR_FACULTIES.dat"), true);
 
-        // courses inside selangor will be inside here
+        # courses inside selangor will be inside here
         $courses = [];
 
-        // referer
+        # referer - later use to get courses that mapped with faculty
         $courses_referer = [];
 
         for ($i = 0; $i < count($selangor); $i++) {
@@ -62,12 +67,12 @@ function icress_getFaculty($faculty) {
             $courses = array_merge($courses, $out[1]);
         }
 
-        // save selangor's faculties
+        # cache selangor's faculties
         file_put_contents("./cache/SELANGOR_REFERER.dat", json_encode($courses_referer));
 
         return json_encode($courses);
 
-    } else { // for non-selangor uitm
+    } else { # for non-selangor uitm
 
         $get = file_get_contents("http://" . ICRESS_URL . "/jadual/{$faculty}/{$faculty}.html");
         $http_response_header or die("Alert_Error: Icress timeout! Please try again later."); 
@@ -81,13 +86,17 @@ function icress_getFaculty($faculty) {
 function icress_getSubject($faculty, $subject) {
 
     $subjects_output = [];
-    // if UiTM inside selangor, then find the correct faculty
+
+    # if UiTM inside selangor, then find the correct faculty
     if ($faculty == 'B') {
 
         $referer = json_decode(file_get_contents("./cache/SELANGOR_REFERER.dat"), true);
         foreach ($referer as $faculties => $subjects) {
             if (in_array($subject, $subjects)) {
                 $curr = icress_getSubject_wrapper($faculties, $subject);
+
+                # if multiple faculties contain the same subject 
+                # then choose one with the highest amount of classes
                 if (count($curr) > count($subjects_output)) {
                     $subjects_output = $curr;
                 }
@@ -103,14 +112,15 @@ function icress_getSubject($faculty, $subject) {
 
 function icress_getSubject_wrapper($faculty, $subject) {
 
-    //start fetch icress data
+    # start fetching the icress data
     $jadual = file_get_contents("http://" . ICRESS_URL . "/jadual/{$faculty}/{$subject}.html");
     $http_response_header or die("Alert_Error: Icress timeout! Please try again later."); 
 
+    # parse the html to more neat representation about classes
     $jadual = str_replace(array("\r", "\n"), '', $jadual);
     preg_match_all('#<td>(.*?)</td>#i', $jadual, $outs);
     $splits = array_chunk(array_splice($outs[1], 7), 7);
-    $new = array();
+    $new = [];
 
     foreach ($splits as $split) {
         $new[$split[0]][] = $split;
