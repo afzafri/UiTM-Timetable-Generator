2d596f323d531413c7ac0713c949a1625a3d76a3
Afif Zafri <zafri_21596@yahoo.com>
2019-02-26 10:23:19 +0800

    decode the json and test access the data

--- modules/exportexcel_module.php
+++ modules/exportexcel_module.php
@@ -3,7 +3,16 @@
 class ExportExcel {
 
   public function toExcel($data) {
-      return $data;
+      $timetable = json_decode($data, true);
+
+      $day = $timetable[0]['day'];
+      $subject = $timetable[0]['subject'];
+      $group = $timetable[0]['group'];
+      $classroom = $timetable[0]['classroom'];
+      $class_start = $timetable[0]['class_start'];
+      $class_end = $timetable[0]['class_end'];
+
+      return $subject;
   }
 
 }
